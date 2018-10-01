904c57fb094fe813b67cd0195e351a04acab17c8
Matt Smith <mattsmithereens@gmail.com>
2018-10-01 16:19:45 -0700

    added styling

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,8 +3,19 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Error404(props){
+
+  const divStyle = {
+    fontFamily: "sans-serif",
+    border: "1px solid grey",
+    width: "50%",
+    margin: "1.25%",
+    padding: "10px",
+    verticalAlign: "top",
+    overflow: "auto"
+  };
+
   return (
-    <div>
+    <div style={divStyle}>
       <h2>The page {props.location.pathname} does not exist!</h2>
       <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
     </div>
