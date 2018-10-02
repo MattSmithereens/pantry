5db7352b5cf3e4a1aa30a45091a107e8bf68e8b3
Matt Smith <mattsmithereens@gmail.com>
2018-10-02 09:03:48 -0700

    took out border

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -6,12 +6,6 @@ function Error404(props){
 
   const divStyle = {
     fontFamily: 'sans-serif',
-    border: '1px solid grey',
-    width: '50%',
-    margin: '1.25%',
-    padding: '10px',
-    verticalAlign: 'top',
-    overflow: 'auto'
   };
 
   return (
