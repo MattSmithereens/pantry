8a54b9b1d47d3ae766995710f8e21b04435c28d6
Matt Smith <mattsmithereens@gmail.com>
2018-10-01 18:39:41 -0700

    added styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(){
+
+  const navStyle = {
+    fontFamily: "sans-serif",
+    padding: "10px",
+    verticalAlign: "top",
+    backgroundColor: '#999'
+  };
+
   return (
-    <div>
+    <div style={navStyle}>
       <h1>Pantry Items</h1>
       <Link to="/">Home</Link> | <Link to="/newfood">Enter New Food Item</Link> | <Link to="/admin">Item Detail</Link>
     </div>
